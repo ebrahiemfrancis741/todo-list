7ede2a988fa3281274351032f417546862e173c3
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-08 03:13:55 +0200

    Add todoList.js
    Add createProject and createTask factory functions that return a new
    project and task respectively. Export both

---
+++ src/todoList.js
@@ -0,0 +1,26 @@
+
+// groups related tasks together
+function createProject(title){
+  return {
+    title,
+  }
+}
+
+// represents a single task 
+function createTask
+(
+  title = "New Task", 
+  dueDate = "undefined",
+  priority = "low",
+  notes = "undefined"
+){
+    return {
+      title,
+      dueDate,
+      priority,
+      notes,
+      complete: false
+    }
+}
+
+export {createProject, createTask};
